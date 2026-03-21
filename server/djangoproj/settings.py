0ddb2d87adbdd1f0bde99fe292fdefe3c3ff6b76
DonnyDaniel <donny@outlook.es>
2026-03-21 13:26:47 -0500

    Refactor allowed hosts and CSRF trusted origins

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://donnydaniel-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://donnydaniel-8000.theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://donnydaniel-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://donnydaniel-8000.theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
